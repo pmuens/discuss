6dda7ab4ff5994a2edc361661f842ac0bb85af4e
Philipp Muens <raute1337@gmx.de>
2016-05-07 11:34:32 +0200

    Add fix for redirect after sign up

--- client/src/app/js/actions/users.js
+++ client/src/app/js/actions/users.js
@@ -71,7 +71,7 @@ export function signIn(user) {
     .then(json => _.isEmpty(json.errors) ? json : Promise.reject(json.errors[0]))
     .then(payload => {
       dispatch({ payload, type: SIGN_IN });
-      dispatch(push(window.previousLocation ? window.previousLocation : '/'));
+      dispatch(push(window.previousLocation && window.previousLocation.pathname !== '/sign-up' ? window.previousLocation : '/'));
     })
     .catch(exception => dispatch({
       type: ERROR,
